a159930b2bbc1abcb69d28c0399d81bc004d521c
Rick de Jong <rjong14@student.avans.nl>
2015-12-12 21:48:25 +0100

    database fix

--- BeerShop/Migrations/Configuration.cs
+++ BeerShop/Migrations/Configuration.cs
@@ -21,9 +21,17 @@ namespace BeerShop.Migrations
             //  to avoid creating duplicate seed data. E.g.
             //
 
+            context.Countries.AddOrUpdate(c => c.Name,
+                new Models.Country() { ID = 1, Name = "The Netherlands" }
+            );
+
+            context.Types.AddOrUpdate(t => t.Name,
+                  new Models.Type() { ID = 1, Name = "Pils" }
+               );
+
             context.Beers.AddOrUpdate(b => b.Name,
-              new Models.Beer() { Name = "Heineken", Price = 2.20 },
-              new Models.Beer() { Name = "Hertog Jan", Price = 3.20 }
+              new Models.Beer() { Name = "Heineken", Price = 2.20, Country_ID = 1, Type_ID = 1},
+              new Models.Beer() { Name = "Hertog Jan", Price = 3.20, Country_ID = 1, Type_ID = 1}
 
 
             );
